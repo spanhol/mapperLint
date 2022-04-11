ce84434b09ad5aebee188bcf5616e4b5a0cf7892
Fernando <fernando.spanhol@viasoft.com.br>
2022-04-11 11:51:40 -0300

    parametro para pasta de saida; parametro para escolher se remove fontes; parametro para escolher se remove cores;

--- mapperLint.js
+++ mapperLint.js
@@ -1,54 +1,98 @@
 #!/usr/bin/env node
 
-let fs = require('fs');
-let xmlConverter = require('xml-js');
+const fs = require('graceful-fs')
+const path = require('path');
+const process = require('process');
+
+const xmlConverter = require('xml-js');
+const options = {
+    basePathEntrada: "",
+    basePathSaida: "",
+    desviarSaida: false,
+    fontes: false,
+    cor: false,
+    validaChamadas: false
+}
 
 function main() {
     const args = process.argv.slice(2);
-    if (!args[0]){
+    if (!args[0]) {
         console.log("Informe um arquivo ou pasta.")
         return;
     }
-    let fileName = args[0]
-    if (!fileName.startsWith("/") && !fileName.startsWith(".")) {
-        fileName = "./" + fileName;
+    options.basePathEntrada = path.join(__dirname)
+    options.basePathSaida = path.join(__dirname)
+
+    for (let i = 1; i < args.length; i++) {
+        const arg = args[i];
+        switch (arg) {
+            case "-d":
+                options.desviarSaida = true;
+                i++
+                options.basePathSaida = path.resolve(args[i]);
+                break;
+            case "-f":
+                options.fontes = true;
+                break;
+            case "-c":
+                options.cor = true;
+                break;
+        }
     }
-    processa("", fileName);
+    options.basePathEntrada = path.resolve(args[0])
+    processa("");
+}
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
 }
 
 main();
 
-function processa(basePath, path) {
-    let caminho = basePath + path;
-    let lsStat = fs.lstatSync(caminho);
-    if (lsStat.isFile() && caminho.endsWith(".mm")) {
+function processa(caminho) {
+    if (caminho.endsWith(".git")) {
+        return
+    }
+    let pathAbsoluto = path.join(options.basePathEntrada, caminho)
+    let lsStat = fs.lstatSync(pathAbsoluto);
+    if (lsStat.isFile() && pathAbsoluto.endsWith(".mm")) {
         processaArquivo(caminho);
     } else if (lsStat.isDirectory()) {
-        fs.readdirSync(caminho).forEach(f => {
-            // console.log(basePath + path + "/" +  f);
-            processa(basePath + path + "/", f);
+        criaPastaSaida(caminho)
+        fs.readdirSync(pathAbsoluto).forEach(file => {
+            processa(caminho + "/" + file);
         });
     }
 }
 
-function processaArquivo(file) {
-    // console.log('\tfile: ', file);
-    fs.readFile(file, function (err, data) {
-        if (file === './arquivos/fiscal/fiscal-beans-stage-four.mm'){
-            console.log("a");
+function criaPastaSaida(arquivo) {
+    if (options.desviarSaida) {
+        let err = fs.mkdirSync(path.join(options.basePathSaida, arquivo), { recursive: true })
+        if (err) {
+            return console.error(err);
         }
+    }
+}
 
+function processaArquivo(file) {
+    fs.readFile(path.join(options.basePathEntrada, file), function (err, data) {
         var js = xmlConverter.xml2js(data, { compact: true, spaces: 4 });
         traverse(js.map);
         let xml = xmlConverter.js2xml(js, { compact: true, spaces: 4, fullTagEmptyElement: true });
-        // xml = xml.replace(/&&/g, '&amp;&amp;');
         xml = xml.replace(/&(?![A-Za-z]+;|#[0-9]+;)/g, '&amp;');
         xml = xml.replace(/\>\<\/icon\>/g, '/>');
         xml = xml.replace(/\>\<\/node\>/g, '>\n</node>');
-        let errWriteFile = fs.writeFileSync(file, xml, 'utf8');
-        if (errWriteFile) {
-            console.log(errWriteFile);
+        let outFile = path.join(options.basePathEntrada, file);
+        if (options.desviarSaida) {
+            outFile = path.join(options.basePathSaida, file);
         }
+        fs.writeFile(outFile, xml, (err) => {
+            if (err) {
+                console.error(err);
+            }
+        })
     });
 }
 
@@ -56,7 +100,7 @@ function traverse(json) {
     if (!json) {
         return;
     }
-    removeAttributos(json);
+    removeAttributos(json, options);
     Object.keys(json).forEach(key => {
         if (key === 'node' && typeof json[key] === 'object') {
             removeAttributos(json[key]);
@@ -76,17 +120,19 @@ function removeAttributos(json) {
         if (json._attributes.MODIFIED) {
             delete json._attributes.MODIFIED;
         }
+        if (options.fontes) {
+            if (json._attributes.FONT) {
+                delete json._attributes.FONT;
+            }
+        }
+        if (options.cor) {
+            if (json._attributes.COLOR) {
+                delete json._attributes.COLOR;
+            }
+        }
     }
 }
 
-function removeFontes(json) {
-
-}
-
-function removeCor(json) {
-
-}
-
 // function validaWritePattern(json) {
-
+    //TODO
 // }
